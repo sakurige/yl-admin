0a1a60d3d2cd562f1143dd544fa5f9ee348041ef
Sakurige <2017782140@qq.com>
2022-10-26 19:56:04 +0800

    chore(custom): 项目初始化，并配置eslint与提交规范

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ["react-app"],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    "no-unused-vars": "warn",
+  },
+  overrides: [],
+};
