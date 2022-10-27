1ed5ead3abae4087f854df99ff776f942e24c8db
Sakurige <2017782140@qq.com>
2022-10-27 20:39:10 +0800

    chore(custom): 补充一些注释

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,6 +1,8 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import type { AppDispatch, RootState } from "./index";
 
+//自定义hook，调用该方法返回一个 dispatch，用于派发action
 const useAppDispatch = () => useDispatch<AppDispatch>();
+// 为rootstate，创建有更好的类型校验的 useSelector，之后用该方法代替 useSelector就好
 const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export { useAppDispatch, useAppSelector };
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import mainReducer from "./reducer/mainReducer";
 
 const store = configureStore({
+  // 创建 store
   reducer: {
     main: mainReducer,
   },
 });
+// 获取到根的state类型，方便后面使用
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export default store;
